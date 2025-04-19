5fb489753c2d9aa387947227a0c2a807f4b8d934
ParpiboyevXurshidbek02 <162950670+ParpiboyevXurshidbek02@users.noreply.github.com>
2025-04-19 17:22:01 +0500

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AnimatedBackground from "./Components/Background";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <AnimatedBackground />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
